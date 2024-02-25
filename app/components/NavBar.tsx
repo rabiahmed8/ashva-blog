05d83cf5bd9a81ced3a20bafe5e7764855a61117
Rabi Ahmed Home <ahmedrabi8@gmail.com>
2024-02-25 07:01:14 +0500

    links fixed

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -11,18 +11,25 @@ const Navbar = () => {
     {
       id: 1,
       link: "home",
+      href:'/'
     },
     {
       id: 2,
       link: "about",
+      href:'/'
+
     },
     {
       id: 3,
       link: "blog",
+      href:'/blog'
+
     },
     {
       id: 4,
       link: "contact",
+      href:'/'
+
     },
   ];
 
@@ -39,12 +46,12 @@ const Navbar = () => {
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map(({ id, link }) => (
+          {links.map(({ id, link , href}) => (
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
-              <Link onClick={() => setNav(!nav)} href={link}>
+              <Link onClick={() => setNav(!nav)} href={href}>
                 {link}
               </Link>
             </li>
